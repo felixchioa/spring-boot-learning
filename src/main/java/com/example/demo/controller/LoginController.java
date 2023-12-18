cf85b5a9c04d37a477a4c0704552828b8d9dda64
趙峰 <feng.zhao1@pactera.com>
2023-12-18 21:47:50 +0800

    add validation to welcome and fail

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,37 +1,34 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserData;
-import org.apache.catalina.User;
+import com.example.demo.dto.LoginDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "goToLoginPage")
-    public String toLoginPage(Model model) {
-        List<String> usernames = new ArrayList<>();
-        usernames.add("LiSa");
-        usernames.add("Joey");
-        usernames.add("Sam");
-        model.addAttribute("usernames", usernames);
-        model.addAttribute("pageTitle", "LoginPage");
-        return "login.html";
+    @GetMapping("/")
+    public ModelAndView loginPage() {
+        return new ModelAndView("login", "loginDto", new LoginDto());
     }
 
-    @PostMapping(value = "login")
-    public String toLoginPage(@ModelAttribute UserData user) {
-        System.out.println(user.getUsername());
-        System.out.println(user.getPassword());
-        return "login.html";
+    @PostMapping("/login")
+    public String login(LoginDto loginDto) {
+        if ("user".equals(loginDto.getUsername()) && "pwd".equals(loginDto.getPassword())) {
+            return "redirect:/welcome";
+        }
+        return "redirect:/fail";
     }
 
+    @GetMapping("/welcome")
+    public String welcomePage() {
+        return "welcome";
+    }
+
+    @GetMapping("/fail")
+    public String failPage() {
+        return "fail";
+    }
 }
