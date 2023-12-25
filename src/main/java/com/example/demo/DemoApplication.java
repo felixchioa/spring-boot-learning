0e394753a740304a3eb639f44e5e13e49d66e6ee
趙峰 <feng.zhao1@pactera.com>
2023-12-25 23:18:06 +0800

    Update dependencies and exclude DataSourceAutoConfiguration Update application.yml configuration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * This is the main class for the demo application.
+ * 
  * @author: FoneZzz
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
     public static void main(String[] args) {
